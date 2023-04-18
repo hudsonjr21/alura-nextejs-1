1cd4aca17b71924e0e20f32008d10c9140e5b3b1
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-04-18 14:08:57 -0300

    capitulo 1

---
+++ pages/faq.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function FAQPage() {
+    return (
+        <div>
+            <h1>
+                Alura Cases - Paginas de Perguntas
+            </h1>
+                <Link href="/">
+                   Ir para Home
+                </Link>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+
+export default function HomePage() {
+    return (
+        <div>
+            <h1>
+                Alura Cases - Home
+            </h1>
+            <Link href="/faq">
+                Ir para FAQ
+            </Link>
+        </div>
+    )
+}
